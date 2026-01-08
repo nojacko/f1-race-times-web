be3d6979f539b0a7ad0edefa5aefc727b4bf9706
James Jackson <nojacko@gmail.com>
2026-01-08 06:12:20 +0000

    Upgrade eslint and all packages

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const prettier = require("eslint-plugin-prettier");
+const prettierConfig = require("eslint-config-prettier");
+
+module.exports = [
+  {
+    ignores: ["public-expo/**", "node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      prettier: prettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
